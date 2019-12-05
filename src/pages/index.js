fec87b94202acffc71d8b339bf558ba4b7d93423
Laura Badgley <l.badgleyy@gmail.com>
2019-12-05 12:50:05 -0800

    incorporating styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const MainImageStyle = styled.div`
+  max-width: 300px;
+  margin-bottom: 1.45rem;
+`
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Welcome To My Page</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <MainImageStyle >
       <Image />
-    </div>
+    </MainImageStyle>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+ 
